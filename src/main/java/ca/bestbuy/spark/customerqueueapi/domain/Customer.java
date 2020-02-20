d259e6c9202ee2f6f22936a48dd5f2e6db8592b9
Gao <egao@macc02s21pmg8wn.ca.bestbuy.com>
2020-02-20 14:55:49 -0800

    updated pom

---
+++ src/main/java/ca/bestbuy/spark/customerqueueapi/domain/Customer.java
@@ -0,0 +1,10 @@
+package ca.bestbuy.spark.customerqueueapi.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.ALWAYS)
+public class Customer {
+
+}
