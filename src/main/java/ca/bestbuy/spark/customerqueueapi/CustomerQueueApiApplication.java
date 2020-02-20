d3198407ebeb312c3d789d529a867f611337be10
Gao <egao@macc02s21pmg8wn.ca.bestbuy.com>
2020-02-20 14:36:01 -0800

    initial commit

---
+++ src/main/java/ca/bestbuy/spark/customerqueueapi/CustomerQueueApiApplication.java
@@ -0,0 +1,13 @@
+package ca.bestbuy.spark.customerqueueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerQueueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerQueueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/bestbuy/spark/customerqueueapi/CustomerQueueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bestbuy.spark.customerqueueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerQueueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
