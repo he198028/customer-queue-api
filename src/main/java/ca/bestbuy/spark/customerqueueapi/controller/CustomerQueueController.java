75af51a5a1b9644611e469fb24338ed5290db059
Gao <egao@macc02s21pmg8wn.ca.bestbuy.com>
2020-02-20 16:42:03 -0800

    updated sms

--- src/main/java/ca/bestbuy/spark/customerqueueapi/controller/CustomerQueueController.java
+++ src/main/java/ca/bestbuy/spark/customerqueueapi/controller/CustomerQueueController.java
@@ -2,15 +2,11 @@ package ca.bestbuy.spark.customerqueueapi.controller;
 
 
 import ca.bestbuy.spark.customerqueueapi.domain.Customer;
-import static ca.bestbuy.spark.customerqueueapi.domain.Constants.*;
 import java.util.List;
 
 import ca.bestbuy.spark.customerqueueapi.service.QueueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
 
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.POST})
@@ -20,8 +16,6 @@ public class CustomerQueueController {
     @Autowired
     public CustomerQueueController(QueueService queueService) {
         this.queueService = queueService;
-
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 
     @GetMapping(value = "/customers")
@@ -31,10 +25,6 @@ public class CustomerQueueController {
 
     @PutMapping(value = "/customers")
     public boolean addToQueue(@RequestBody Customer customer) {
-    	
-    	Message.creator(new PhoneNumber(customer.getPhone()),new PhoneNumber(SENDERS_PHONE),
-    			"Welcome " + customer.getName()+ " an associate we'll be with you shortly.").create();
-    	
         return queueService.AddToQueue(customer);
     }
 
--- src/main/java/ca/bestbuy/spark/customerqueueapi/service/QueueServiceImpl.java
+++ src/main/java/ca/bestbuy/spark/customerqueueapi/service/QueueServiceImpl.java
@@ -1,6 +1,7 @@
 package ca.bestbuy.spark.customerqueueapi.service;
 
 import ca.bestbuy.spark.customerqueueapi.domain.Customer;
+import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static ca.bestbuy.spark.customerqueueapi.domain.Constants.SENDERS_PHONE;
+import static ca.bestbuy.spark.customerqueueapi.domain.Constants.*;
 
 @Service
 public class QueueServiceImpl implements QueueService {
@@ -21,12 +22,18 @@ public class QueueServiceImpl implements QueueService {
 
     public QueueServiceImpl() {
         queue = new LinkedBlockingDeque();
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 
     @Override
     public boolean AddToQueue(Customer customer) {
         try {
             queue.put(customer);
+
+            Message.creator(new PhoneNumber(customer.getPhone()),new PhoneNumber(SENDERS_PHONE),
+                    "Welcome " + customer.getName()+ " an associate we'll be with you shortly.").create();
+
             return true;
         } catch(InterruptedException e) {
             return false;
